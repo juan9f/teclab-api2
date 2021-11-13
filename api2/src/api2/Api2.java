363dd133d7fa5c65a5aa0e629e132d4cca16a03b
juan Ibarra <juanibarra.79@gmail.com>
2021-11-13 09:43:54 -0300

    api 2 doc

--- api2/src/api2/Api2.java
+++ api2/src/api2/Api2.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package api2;
 import java.util.Scanner;
 
-/**
- *
- * @author jaifa
- */
 public class Api2 {
 
     /**
